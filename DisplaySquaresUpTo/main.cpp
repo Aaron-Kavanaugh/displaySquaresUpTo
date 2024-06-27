4fa3b85dd682b1aa0427a98011d64a65947977e0
Aaron Kavanaugh <aaron.kavanaugh@outlook.com>
2024-06-27 15:18:38 -0500

    Initial commit

---
+++ DisplaySquaresUpTo/main.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+//should be the fastest and most efficient, as it has no need to call upon sqrt.
+void DisplaySquares(unsigned int n) {
+    for (int i = 0; i < 10; ++i) {
+        cout << i << " * " << i << " = " << i * i << endl;
+    }
+}
+
+void DisplaySquaresUpTo(unsigned int n)
+{
+    unsigned int i;
+    unsigned int limit = sqrt(100); // Calculate the square root of 100 once
+
+    /*  This outputs all numbers whose square is < 100, i.e. it will show:  0 * 0 = 0
+                                                                            1 * 1 = 1
+                                                                            2 * 2 = 4
+                                                                            3 * 3 = 9
+                                                                            ...
+                                                                            9 * 9 = 81
+    */
+
+    cout << "Here are some squares <100 : " << endl;
+    for (i = 0; i <= limit && i < n; i++)
+    {
+        
+        cout << i << " * " << i << " = " << i * i << endl;
+    }
+
+    // The FOR loop above isn't very efficient. How can it be improved?
+    // Hint: sqrt() is computationally expensive (it takes a long time).
+
+    cout << endl;
+
+}
+
+int main() {
+
+    unsigned int n{ 10 };
+
+    //DisplaySquaresUpTo(n);
+
+    DisplaySquares(n);
+
+	return 0;
+}
